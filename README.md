bf3c09f1ed5a555e59136be373ad61a30ed3504f
Chris Lundie <chris@lundie.ca>
2015-04-29 20:48:09 -0400

    Add CodeMirror to View2

--- README.md
+++ README.md
@@ -15,6 +15,14 @@ Demo project that shows Webpack code splitting with React.
   `.js` resources, so the user gets the benefit of not downloading styles until
   needed.
 
+- `View2` has a dependency on CodeMirror, which is quite large. Notice how
+  all the JavaScript and CSS for CodeMirror gets bundled along with the
+  `View2` component, and so does not load until the user never navigates
+  there.
+
+- In the browser, notice how `script` and `style` elements are inserted into
+  `head` as the various components are loaded for the first time.
+
 See:
 
 react-proxy-loader:
--- src/view/View2.jsx
+++ src/view/View2.jsx
@@ -1,14 +1,51 @@
+const CodeMirror = require("codemirror");
 const React = require("react");
 require("./View2.less");
+require("codemirror/mode/htmlmixed/htmlmixed.js");
 
 class View2 extends React.Component {
 
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
-      <h2 id="view2">
-        View2
-      </h2>
+      <div id="view2">
+        <h2>
+          View2
+        </h2>
+        <div ref="editor"></div>
+      </div>
     );
+  }
+
+  componentDidMount() {
+    this._createEditor();
+  }
+
+  componentWillUnmount() {
+    this._destroyEditor();
+  }
+
+  _createEditor() {
+    const container = this.refs.editor.getDOMNode();
+    Array.from(container.childNodes).forEach((node) => {
+      node.remove()
+    });
+    this._editor = new CodeMirror(container, {
+      mode: "htmlmixed",
+      lineNumbers: true,
+      viewportMargin: 0,
+      lineWrapping: true,
+    });
+  }
+
+  _destroyEditor() {
+    const {_editor} = this;
+    if (_editor) {
+      delete this._editor;
+    }
   }
 
 }
